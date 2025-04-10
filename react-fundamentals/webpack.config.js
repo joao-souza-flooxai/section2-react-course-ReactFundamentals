534366ec67e003605de67bb9f999ecc17bdba695
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-10 10:49:36 -0300

    fix: Fixed dependency problems.

--- react-fundamentals/webpack.config.js
+++ react-fundamentals/webpack.config.js
@@ -1,18 +1,26 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname + '/public',
-    filename: './bundle.js'
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './public',
-    port: 1000
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 1000,
   },
   module: {
-    loaders: [{
-      test: /.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
   }
-}
+};
